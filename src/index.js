f9f6e7f24726d364dbbed0db9bf85b5d4f1a0369
Alexander Jones <89351310+AlexJonesCodes@users.noreply.github.com>
2024-09-01 18:03:19 +0100

    added meta tags

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+<><meta name="description" content="The most delicious restaurant in town" />
+<meta name="og:title" content="Little Lemon" />
+<meta name="og:description" content="The website for the fictional Little Lemon restaurant"/>
+<meta name="og:image" content="https://greyhatguy007.github.io/little-lemon-coursera/data/little-lemon-logos/Asset-16@4x.png"/></>
+
 root.render(
   <React.StrictMode>
     <App />
