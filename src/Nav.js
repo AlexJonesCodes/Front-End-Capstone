4919b8cf159315909c7b228852517f9963c7b94c
Alexander Jones <89351310+AlexJonesCodes@users.noreply.github.com>
2024-09-01 22:36:32 +0100

    beginning of css

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './App.css';
 
 function Nav() {
 return (
 <nav>
-    <img src="logo.jpg" alt="Little Lemon logo" />
-    <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#menu">Menu</a></li>
-        <li><a href="#reservation">Reservations</a></li>
-        <li><a href="#order">Order Online</a></li>
-        <li><a href="#login">Login</a></li>
-    </ul>
+    <div className="nav-container">
+        
+        <ul>
+            <img src="https://rhas92.github.io/Littlelemon/images/Logofooter1.png" alt="Little Lemon logo" />
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservation">Reservations</a></li>
+            <li><a href="#order">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+        </ul>
+    </div>
+    
 </nav>
 );
 }
