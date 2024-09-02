de1960c6974cd21fb49e14adc7f74c8ef3ba283f
Alexander Jones <89351310+AlexJonesCodes@users.noreply.github.com>
2024-09-02 18:46:02 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
-import Nav from './Nav';
 
 function Footer() {
 return (
     <footer>
-        <Nav />
+        <div className="footer-container">
+        <div className="footer-logo">
+            <img src="https://rhas92.github.io/Littlelemon/images/Logofooter1.png" alt="Little Lemon logo" />
+        </div>
+        <div className="footer-nav-container">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#reservation">Reservations</a></li>
+                <li><a href="#order">Order Online</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
+        </div>
+
+        <div className="footer-contact">
+            <h2>Contact Us</h2>
+            <p>123-456-7890</p>
+            <p>littlelemon@meta.com</p>
+        </div>
+
+        <div className="footer-social">
+            <h2>Follow Us</h2>
+            <ul>
+                <li><a href="https://www.facebook.com">Facebook</a></li>
+                <li><a href="https://www.twitter.com">Twitter</a></li>
+                <li><a href="https://www.instagram.com">Instagram</a></li>
+            </ul>
+        </div>
+        </div>
         
+        
+
         <p>&copy; 2024 Little Lemon</p>
     </footer>
 );
